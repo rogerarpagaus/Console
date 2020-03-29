fa92862f89363fb8b89f6601b0ddc20ba4a9ae26
rogerarpagaus <46073270+rogerarpagaus@users.noreply.github.com>
2020-03-29 20:52:13 +0200

    Update LCDcon.py

--- LCDcon.py
+++ LCDcon.py
@@ -7,16 +7,11 @@ from PIL import ImageFont
 import time
 
 import socket
-import fcntl
-import struct
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print(s.getsockname()[0])
+s.close()
 
 spiSettings = SPI.SpiDev(0,0, max_speed_hz=4000000)
 d = LCD.PCD8544(23, 24, spi=spiSettings)
@@ -33,7 +28,7 @@ draw.rectangle((35,2,54,22), outline=0, fill=255)
 draw.polygon([(63,33), (73,2), (83,22)], outline=0, fill=255)
 
 font = ImageFont.load_default()
-draw.text ((8,30), get_ip_address('wlan0'), font=font)
+draw.text ((8,30), "", font=font)
 
 d.image(image)
 d.display()

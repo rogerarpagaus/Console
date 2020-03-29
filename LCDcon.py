f67d5a6c5e71816905f8500d599fc9721f4a31f8
rogerarpagaus <46073270+rogerarpagaus@users.noreply.github.com>
2020-03-29 21:03:17 +0200

    Update LCDcon.py

--- LCDcon.py
+++ LCDcon.py
@@ -25,6 +25,7 @@ draw = ImageDraw.Draw(image)
 #draw.rectangle((0,0,84,48), outline=255, fill=255)
 draw.rectangle((0,0,83,47), outline=0, fill=255)
 font = ImageFont.load_default()
+draw.text ((1,20), "IP adr:", font=font)
 draw.text ((1,30), ip, font=font)
 
 d.image(image)
